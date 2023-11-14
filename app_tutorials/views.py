851815909fb42043a72a847552317e858e9d73be
Arun R G <arunrg.agility@gmail.com>
2023-11-14 23:02:52 +0530

    Release build: v1.0.4 Python Tutorials, MD syntax, home page

--- app_tutorials/views.py
+++ app_tutorials/views.py
@@ -19,8 +19,13 @@ def tutorials_home_page(request):
 
 
 def test_markdown(request):
+    os_info = platform.system()
+    print(f">>> === OS INFO: {os_info} === <<<")
+    prod_path = ""
+    if os_info.find("Linux") != -1:
+        prod_path = f"/home/DEVAGILEAGILITY/com_learnpythondjango/lpdcom/django_edu/"
     # File path to the Markdown file in the template directory
-    markdown_file_path = 'app_tutorials/templates/app_tutorials/md_content/test_md.md'
+    markdown_file_path = f"{prod_path}{app_name}/templates/{app_name}/md_content/test_md.md"
     # Read the content of the Markdown file
     html_content = ""
     with open(markdown_file_path, 'r') as file:
