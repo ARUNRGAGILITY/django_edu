bbd55de6be6a3036527a6a88146539f0f4018d72
Arun R G <arunrg.agility@gmail.com>
2023-11-17 00:31:59 +0530

    Added Tutorials

--- app_tutorials/urls.py
+++ app_tutorials/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('tutorials_home_page', views.tutorials_home_page, name="tutorials_home_page"),
     path('test_markdown', views.test_markdown, name="test_markdown"),
     
+    # Django Foundation course1
+    path('django_course1', views.django_course1, name="django_course1"),
+    
     # display_md_topic
     path('display_md_topic/<str:topic>', views.display_md_topic, name="display_md_topic"),
+    # display_md_sub_topic
+    path('display_md_sub_topic/<str:folder>/<str:topic>', views.display_md_sub_topic, name="display_md_sub_topic"),
 ]
--- app_tutorials/views.py
+++ app_tutorials/views.py
@@ -16,6 +16,17 @@ def tutorials_home_page(request):
     template_file = f"{app_name}/tutorials_home.html"
     return render(request, template_file, context)
 
+def django_course1(request):
+    #
+    
+    
+    
+    #
+    context = {
+        'page': "django_course1",
+    }
+    template_file = f"{app_name}/django_course1.html"
+    return render(request, template_file, context)
 
 
 def test_markdown(request):
@@ -37,7 +48,7 @@ def test_markdown(request):
     return render(request, template_file, context)
 
 
-def display_md_topic(request, topic):
+def display_md_topic(request, topic, sub_topic=False):
     os_info = platform.system()
     print(f">>> === OS INFO: {os_info} === <<<")
     prod_path = ""
@@ -53,6 +64,27 @@ def display_md_topic(request, topic):
         markdown_content = file.read()
     context = {'html_content': markdown_content,
                'page': 'tutorials',
-               'topic': topic,}
+               'topic': topic,
+               'sub_topic': sub_topic}
     template_file = f"{app_name}/display_md_topic.html"
+    return render(request, template_file, context)
+
+def display_md_sub_topic(request, folder, topic):
+    os_info = platform.system()
+    prod_path = ""
+    if os_info.find("Linux") != -1:
+        prod_path = f"/home/DEVAGILEAGILITY/com_learnpythondjango/lpdcom/django_edu/"
+    # File path to the Markdown file in the template directory
+    markdown_file_path = f"{prod_path}{app_name}/templates/{app_name}/md_content/{folder}/{topic}.md"
+    # Print the file path for debugging
+
+    # Read the content of the Markdown file
+    html_content = ""
+    with open(markdown_file_path, 'r') as file:
+        markdown_content = file.read()
+    context = {'html_content': markdown_content,
+               'page': 'tutorials',
+               'topic': topic,
+                }
+    template_file = f"{app_name}/display_md_sub_topic.html"
     return render(request, template_file, context)
